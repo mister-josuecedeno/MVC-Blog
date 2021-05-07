df27144b2def18acd953abd4c7c6eb18651fea90
mister-josuecedeno <josuecedeno@yahoo.com>
2021-05-07 11:15:43 -0500

    Add Summernote

--- Controllers/BlogsController.cs
+++ Controllers/BlogsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -51,6 +52,11 @@ namespace MVC_Blog.Controllers
         }
 
         // GET: Blogs/Create
+        // [Authorize]
+        // [Authorize(Roles = "Administrator")]
+        // [Authorize(Roles = "Moderator")]
+        // [Authorize(Roles = "Administrator,Moderator")]
+        // [AllowAnonymous] - open to everyone
         public IActionResult Create()
         {
             return View();
