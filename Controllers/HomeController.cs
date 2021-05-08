b0b3375207011e090f6fdf299c53a2d61e61ba2c
mister-josuecedeno <josuecedeno@yahoo.com>
2021-05-08 11:44:07 -0500

    Add default image to home page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using MVC_Blog.Data;
 using MVC_Blog.Models;
+using MVC_Blog.Services;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -15,15 +17,28 @@ namespace MVC_Blog.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly ApplicationDbContext _context;
+        private readonly IFileService _fileService;
+        private readonly IConfiguration _configuration;
 
-        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context, IFileService fileService, IConfiguration configuration)
         {
             _logger = logger;
             _context = context;
+            _fileService = fileService;
+            _configuration = configuration;
         }
 
         public async Task<IActionResult> Index()
         {
+            // Default Home Page Image
+            var DefaultImage = await _fileService.EncodeFileAsync(_configuration["DefaultHomePageImage"]);
+            var DefaultContentType = _configuration["DefaultHomePageImage"].Split('.')[1];
+
+
+            ViewData["HeaderText"] = "Josue's Blog";
+            ViewData["SubText"] = "Helping Coders One Line At A Time";
+            ViewData["HeaderImage"] = _fileService.DecodeImage(DefaultImage, DefaultContentType);
+            
             var allBlogs = await _context.Blogs.ToListAsync();
             return View(allBlogs);
         }
